39eb5d1c6e97394526148f0b1269e2303006c6e1
SriAakash-Github <sriaakashsriaakash@gmail.com>
2025-12-10 10:59:31 +0530

    day7Java

---
+++ day7/java/datatypes.java
@@ -0,0 +1,24 @@
+public class datatypes {
+    public static void main(String[] args) {
+        int integerNumber = 10;
+        double decimalNumber = 20.5;
+        char character = 'A';
+        boolean isTrue = true;
+        short shortNumber = 5;
+        long longNumber = 100000L;
+        float floatNumber = 15.5f;
+        byte byteNumber = 2;
+
+        System.out.println("Data Types in Java:");
+        System.out.println("Short: " + shortNumber);
+        System.out.println("Long: " + longNumber);
+        System.out.println("Float: " + floatNumber);
+        System.out.println("Byte: " + byteNumber);
+
+        
+        System.out.println("Integer: " + integerNumber);
+        System.out.println("Double: " + decimalNumber);
+        System.out.println("Character: " + character);
+        System.out.println("Boolean: " + isTrue);
+    }
+}
---
+++ day7/java/exercise1.java
@@ -0,0 +1,10 @@
+public class exercise1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Difference: " + (a - b));
+        System.out.println("Product: " + (a * b));
+        System.out.println("Quotient: " + (a / b));
+    }
+}
