df66f05269f4cc8bb8f5a42d8f2df4d2481390df
SriAakash-Github <sriaakashsriaakash@gmail.com>
2025-12-12 09:00:22 +0530

    DAy 9

---
+++ day9/prob1.java
@@ -0,0 +1,12 @@
+public class prob1 {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows
+
+        for (int i = 1; i <= n; i++) { // Loop for each row
+            for (int j = 1; j <= i; j++) { // Loop for printing stars in each row
+                System.out.print("* "); // Print star followed by a space
+            }
+            System.out.println(); 
+        }
+    }
+}
